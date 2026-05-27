3ba9eb408ec0facfe0ed0f898757372370e33663
kmkim <kmkimhr@gmail.com>
2026-05-27 06:16:47 +0000

    [FIX] 화면 세로 스크롤 방지를 위해 높이를 줄이는 방향으로 변경

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
 
+function applyMobileHeight() {
+  if (window.innerWidth <= 768) {
+    document.documentElement.style.setProperty('--root-height', `${window.innerHeight - 50}px`);
+  } else {
+    document.documentElement.style.removeProperty('--root-height');
+  }
+}
+applyMobileHeight();
+window.addEventListener('resize', applyMobileHeight);
+
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find root element');
 
