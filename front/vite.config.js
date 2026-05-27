dd478a02e4e2de7d20031f0173118d90580da24c
kmkim <kmkimhr@gmail.com>
2026-05-27 04:47:59 +0000

    [ADD] PWA 아이콘 추가. nginx 캐시 설정 제거

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,8 +1,44 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon-192.png', 'icon-512.png'],
+      manifest: {
+        name: 'Voice With Me',
+        short_name: 'VoiceWithMe',
+        description: '화상 통화 앱',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/kmkim\.space\/socket\.io\//,
+            handler: 'NetworkOnly',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       '/socket.io': {
